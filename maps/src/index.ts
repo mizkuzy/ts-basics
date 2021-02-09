062bcd28804da5e468401f04b9ac825eb639444d
lyudmila.rudkova@kandasoft.com <MisteR2901>
2021-02-09 14:38:54 +0300

    add google maps api and type definition files

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -6,3 +6,11 @@ console.log(user)
 
 const company = new Company()
 console.log(company.catchPhrase)
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+})
