fedfa385635d08f65fd88493a0f930356b4ea4e0
lyudmila.rudkova@kandasoft.com <MisteR2901>
2021-02-10 12:59:51 +0300

    add content to marker

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,12 +1,16 @@
-interface Mappable {
+import InfoWindow = google.maps.InfoWindow
+
+export interface Mappable {
   location: {
     lat: number,
     lng: number
-  }
+  },
+  getMarkerContent():string
 }
 
 export class CustomMap {
   private map: google.maps.Map;
+
 
   constructor(divId: string) {
     this.map = new google.maps.Map(document.getElementById(divId), {
@@ -18,13 +22,20 @@ export class CustomMap {
     })
   }
 
-  addMarker(mappable: Mappable):void {
-    new google.maps.Marker({
-      map:this.map,
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.map,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
+    })
+
+    marker.addListener('click', () => {
+      const infoWindow = new InfoWindow({
+        content: mappable.getMarkerContent()
+      })
+      infoWindow.open(this.map, marker)
     })
   }
 }
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,6 +1,7 @@
 import * as faker from 'faker'
+import {Mappable} from './CustomMap'
 
-export class User {
+export class User implements Mappable{
   name: string;
   location: {
     lat: number,
@@ -13,5 +14,11 @@ export class User {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     }
+  }
+
+  getMarkerContent(): string {
+    return `
+      User name: ${this.name}
+    `;
   }
 }
